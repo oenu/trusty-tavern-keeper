b59e9d65450a53107cd09cb6e3ce387db7fa7575
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-01-23 14:11:23 -0500

    testing

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,25 @@
 import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
+import { selectPhobias } from 'src/features/phobias/phobiasSlice';
+import { useAppSelector } from 'src/redux/hooks';
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
 export function App() {
+  const phobias = useAppSelector(selectPhobias);
+
   return (
     <StyledApp>
-      <NxWelcome title="trusty-tavern-keeper" />
+      {phobias.map((phobia) => (
+        <div key={phobia.id}>
+          <h1>{phobia.name}</h1>
+          <p>{phobia.description}</p>
+        </div>
+      ))}
+
+      {/* <NxWelcome title="trusty-tavern-keeper" /> */}
     </StyledApp>
   );
 }
