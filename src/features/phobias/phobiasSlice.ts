bee4c6a33bf335d8fbe500e58bc8de5c4412ad75
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-01-23 13:48:27 -0500

    redux slices/store

---
+++ src/features/phobias/phobiasSlice.ts
@@ -0,0 +1,66 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from 'src/redux/store';
+
+/**
+ * Phobias are a list of fears that a user might have. Each survey respondent will respond to each phobia with either 0, 1, 2, 3, 4, or 5.
+ * 0 = No Preference/No Response
+ * 1 = Not affected
+ * 2 = Wary (Willing to use this content in the DND game, but only sparingly)
+ * 3 = Concerned (Willing to use this content if a warning is given at the start of the session)
+ * 4 = Uncomfortable (user would prefer not to use this content, but can work around it/with it)
+ * 5 = Avoid (user would prefer not to use this content at all. This overrides all other responses from other users in the party)
+ *
+ * Users can add their own phobias, but they will be added to the end of the list, and will be assigned a new uuid as their id.
+ */
+
+// Define a type for the slice state
+interface PhobiasState {
+  phobias: Phobia[];
+}
+
+// Define a type for the slice state
+export interface Phobia {
+  id: number;
+  name: string;
+  description: string;
+  severity: number;
+}
+
+// Define the initial state using that type
+const initialState: PhobiasState = {
+  phobias: [
+    {
+      id: 1,
+      name: 'Abuse',
+      description: 'Abuse of any kind',
+      severity: 0,
+    },
+    {
+      id: 2,
+      name: 'Abortion',
+      description: 'Abortion',
+      severity: 0,
+    },
+  ],
+};
+
+export const phobiasSlice = createSlice({
+  name: 'phobias',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    addPhobia: (state, action: { payload: Phobia }) => {
+      state.phobias.push(action.payload);
+    },
+  },
+});
+
+// Export reducer actions
+export const { addPhobia } = phobiasSlice.actions;
+
+// Export selectors for the slice state
+export const selectPhobias = (state: RootState) => state.phobias.phobias;
+
+// Export the reducer as a default export
+export default phobiasSlice.reducer;
---
+++ src/features/topics/topicsSlice.ts
@@ -0,0 +1,108 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from 'src/redux/store';
+
+/**
+ * Topics are the questions about how a user wants to play a roleplaying session.
+ * Each topic will have a unique id, a question, and a list of 4 possible answers.
+ * The user will select one of the answers, and the answer will be used to determine the intensity of the game session.
+ * 0 = Skip - The game session will be determined by the intensity of the group and a warning will be displayed when all the players are reviewing the game session.
+ * 1 = Fantasy - The game session will be light-hearted and fun.
+ * 2 = Adventure - The game session will be a little more serious, but still fun.
+ * 3 = Struggle - The game session will be more serious, and will have some dark themes.
+ * 4 = Tragedy - The game session will be very serious, and will have dark themes.
+ *
+ * Options will be shown in the following order: (skip option will be shown along with the other options)
+ * 1. Fantasy
+ * 2. Adventure
+ * 3. Struggle
+ * 4. Tragedy
+ * A player can approve of each option, or by disapproving of the next option they will automatically select the previous option.
+ * (4 buttons, "Less Intense", "More Intense", "Current Intensity", "No Preference")
+ */
+
+// Define a type for the slice state
+interface TopicsState {
+  topics: Topic[];
+}
+
+// Define a type for the slice state
+export interface Topic {
+  id: number; // Unique ID for the topic
+  label: string; // Label for the topic (Combat, Exploration, etc.)
+  question: string; // Question for the topic (You come across a monastery, what do you do?)
+  answers: {
+    Fantasy: string;
+    Adventure: string;
+    Struggle: string;
+    Tragedy: string;
+  };
+  response: number;
+}
+
+// Define the initial state using that type
+const initialState: TopicsState = {
+  topics: [
+    {
+      id: 1,
+      label: 'Combat',
+      question:
+        'You are in a dungeon and a goblin attacks you. What do you do?',
+      answers: {
+        Fantasy: 'I attack the goblin with my sword.',
+        Adventure:
+          'I attack the goblin with my sword, and I yell "For the glory of the king!"',
+        Struggle:
+          'I attack the goblin with my sword, and I yell "For the glory of the king!" as I stab him in the heart.',
+        Tragedy:
+          'I attack the goblin with my sword, and I yell "For the glory of the king!" as I stab him in the heart. I then cut off his tongue and attach it to my necklace of giblets',
+      },
+      response: 0,
+    },
+    {
+      id: 2,
+      label: 'Torture',
+      question:
+        'You are captured by a noble lord, how do they extract information from you',
+      answers: {
+        Fantasy:
+          "The noble lord leaves you in his dungeon for a day, you wish you'd eaten lunch",
+        Adventure:
+          'The noble lord chains you to a wall and leaves you there for a week, a rat nibbles on your toes',
+        Struggle:
+          'The noble lord chains you to a wall and leaves you there for a month, you are covered in flea bites, and you are forced to listen to the screams of other prisoners',
+        Tragedy:
+          "The noble lord chains you to a fetid wall next to a dying man, you are branded with the noble lord's sigil and left to die should you not tell him what he wants to know.",
+      },
+      response: 0,
+    },
+  ],
+};
+
+export const topicsSlice = createSlice({
+  name: 'topics',
+  initialState,
+  reducers: {
+    setTopicResponse: (
+      state,
+      action: { payload: { id: number; response: number } }
+    ) => {
+      const { id, response } = action.payload;
+      const topic = state.topics.find((topic) => topic.id === id);
+      if (topic) {
+        topic.response = response;
+      }
+    },
+    addTopic: (state, action: { payload: Topic }) => {
+      state.topics.push(action.payload);
+    },
+  },
+});
+
+// Export reducer actions
+export const { setTopicResponse, addTopic } = topicsSlice.actions;
+
+// Export selectors for the slice state
+export const selectTopics = (state: RootState) => state.topics.topics;
+
+// Export the reducer as a default export
+export default topicsSlice.reducer;
