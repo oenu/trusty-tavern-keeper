4855d181738f3efd98e5081ef020e38a504b60ae
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-01-24 08:18:52 -0500

    supabase init

--- src/screens/Survey/Survey.tsx
+++ src/screens/Survey/Survey.tsx
@@ -1,16 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function Survey() {
   // Prisma client
-  const prisma = new PrismaClient();
+  // const prisma = new PrismaClient();
 
   // Get the id from the url
   const { id } = useParams<{ id: string }>();
 
   // Get the phobia data - all the phobias
-  const survey = prisma.phobiaList.findMany();
+  // const survey = prisma.phobiaList.findMany();
 
   return <div>Survey id: {id}</div>;
 }
