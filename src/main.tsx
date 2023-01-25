08d2de2f21e992801ad17d4b115e3d814e092b15
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-01-25 07:14:12 -0500

    init mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
 import App from './app/app';
-
-import { setupStore } from './lib/redux/store';
-export const store = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <Provider store={store}>
+    <MantineProvider
+      theme={{ colorScheme: 'dark' }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
+    </MantineProvider>
   </StrictMode>
 );
