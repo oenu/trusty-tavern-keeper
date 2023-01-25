772086fd9bab1857e6694d9778b70c7f860a2b34
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-01-25 07:14:01 -0500

    init mantine and session

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,55 @@
+import { AppShell } from '@mantine/core';
 import styled from 'styled-components';
 
 // App Router
+import { Session } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+import NavHeader from 'src/features/nav/NavHeader';
+
+import { supabase } from 'src/lib/supabase/client';
 import Router from './Router';
 
 const StyledApp = styled.div`
   // Your style here
 `;
-
 export function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const session = await supabase.auth.getSession();
+      if (session.data.session?.user) {
+        console.log(session.data.session);
+        setSession(session.data.session);
+      } else {
+        console.log('no session found');
+      }
+    };
+    checkSession();
+  }, []);
+
+  supabase.auth.onAuthStateChange((_event, session) => {
+    console.log('auth state changed');
+    setSession(session);
+  });
+
   return (
     <StyledApp>
-      <Router />
+      <AppShell
+        padding="md"
+        header={<NavHeader session={session} />}
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        {/* Your application here */}
+        <Router />
+      </AppShell>
     </StyledApp>
   );
 }
