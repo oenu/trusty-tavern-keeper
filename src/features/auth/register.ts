198a3d1136f75be33f913a6a701edabddd98ea17
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-01-25 10:46:51 -0500

    debug

--- src/features/auth/register.ts
+++ src/features/auth/register.ts
@@ -1,4 +1,6 @@
 import { Session } from '@supabase/supabase-js';
+import ky from 'ky';
+import { type APIUser } from 'discord-api-types/v10';
 
 export const handleRegister = async ({
   session,
@@ -16,4 +18,16 @@ export const handleRegister = async ({
     console.debug('handleRegister: No token found');
     throw new Error('No token found');
   }
+
+  // Test discord API to get current user info
+
+  console.debug('handleRegister: Testing discord API to get current user info');
+  const discordUser = (await ky('https://discord.com/api/users/@me', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).json()) as APIUser;
+  console.debug(discordUser.avatar);
+
+  console.debug('handleRegister: discordUser', discordUser);
 };
