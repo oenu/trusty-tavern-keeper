f14332bc5b3f3766eae6c0469e02aba22ee06f2a
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-02-06 04:10:07 -0500

    rebuilt

--- src/app/supabase/client.ts
+++ src/app/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://bmaguvhfgivfjdmapznl.supabase.co';
+const supabaseUrl = 'https://kgmjeynliwbxluitaeys.supabase.co';
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtYWd1dmhmZ2l2ZmpkbWFwem5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ1NDg4MzQsImV4cCI6MTk5MDEyNDgzNH0.kkN9T04Iv5uxSE0my_xpxJ_ZWI_v0-Z7QcJmCZSTyHo';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnbWpleW5saXdieGx1aXRhZXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU0Nzc1MTIsImV4cCI6MTk5MTA1MzUxMn0.WB0aiVcEiJ9hXQBuibN4CQAnoB2AjRjiCPAV5Ywrh8k';
 
 if (!supabaseKey) {
   throw new Error('Missing Supabase key');
