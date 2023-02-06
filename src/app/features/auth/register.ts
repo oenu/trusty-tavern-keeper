752e7f527347bd83b8f028b94c5bf4dd03a23194
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-02-06 04:10:47 -0500

    testfunction

--- src/app/features/auth/register.ts
+++ src/app/features/auth/register.ts
@@ -1,33 +1,36 @@
 import { Session } from '@supabase/supabase-js';
-import ky from 'ky';
-import { type APIUser } from 'discord-api-types/v10';
+// import ky from 'ky';
+// import { type APIUser } from 'discord-api-types/v10';
+import { supabase } from 'src/app/supabase/client';
 
 export const handleRegister = async ({
   session,
 }: {
   session: Session | null;
 }) => {
+  console.log('handleRegister: ', session);
+
   if (!session) {
     console.debug('handleRegister: No session found');
     return;
   }
 
-  const token = session.provider_token;
+  // Test
+  // Add name to supabase
 
-  if (!token) {
-    console.debug('handleRegister: No token found');
-    throw new Error('No token found');
+  const profiles = await supabase.from('profiles').select();
+
+  console.log('handleRegister: profiles', profiles);
+
+  const nameResponse = await supabase
+    .from('public.profiles')
+    .update([{ name: 'testName' }])
+    .select();
+
+  if (nameResponse.error) {
+    console.error('handleRegister: Error creating phobia', nameResponse.error);
+    return;
   }
 
-  // Test discord API to get current user info
-
-  // console.debug('handleRegister: Testing discord API to get current user info');
-  // const discordUser = (await ky('https://discord.com/api/users/@me', {
-  //   headers: {
-  //     Authorization: `Bearer ${token}`,
-  //   },
-  // }).json()) as APIUser;
-  // console.debug(discordUser.avatar);
-
-  // console.debug('handleRegister: discordUser', discordUser);
+  console.log('handleRegister: profile updated', nameResponse.data);
 };
