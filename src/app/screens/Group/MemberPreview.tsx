3338c52d2c5f655f633ca78e698eb50b65c152ef
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-02-08 10:59:18 -0500

    added member preview

---
+++ src/app/screens/Group/MemberPreview.tsx
@@ -0,0 +1,62 @@
+import {
+  Text,
+  Stack,
+  UnstyledButton,
+  Avatar,
+  Divider,
+  Group,
+} from '@mantine/core';
+import { useHover } from '@mantine/hooks';
+import { RxCheck } from 'react-icons/rx';
+import { SiDiscord } from 'react-icons/si';
+
+export default function MemberPreview(member: GroupMember) {
+  const { hovered, ref } = useHover();
+  return (
+    <Stack>
+      <Group position={'apart'}>
+        <Group>
+          <UnstyledButton
+            onClick={() => {
+              window.open(
+                `https://discord.com/users/${member.discord_id}`,
+                '_blank'
+              );
+            }}
+          >
+            <Avatar
+              ref={ref}
+              src={hovered ? undefined : member.profile_picture}
+              alt={member.full_name}
+              size="xl"
+              radius="xl"
+              style={{
+                // Box shadow if owner
+                boxShadow: member.is_owner ? '0 0 5px 6px teal' : undefined,
+              }}
+            >
+              <SiDiscord style={{ width: '100%' }} color={'#5865F2'} />
+            </Avatar>
+          </UnstyledButton>
+
+          <Stack spacing={0}>
+            <Text style={{ fontWeight: 700 }}>{member.full_name}</Text>
+            <Text inline italic align="center">
+              {member.name}
+            </Text>
+          </Stack>
+        </Group>
+        {member.is_owner && (
+          <Text inline color="teal">
+            Owner
+          </Text>
+        )}
+        <Group>
+          <Text style={{ fontWeight: 700 }}>Survey Complete?</Text>
+          <RxCheck size={30} />
+        </Group>
+      </Group>
+      <Divider />
+    </Stack>
+  );
+}
