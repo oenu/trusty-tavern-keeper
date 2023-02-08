f3afed173055d06856c174d5af4718db149e4293
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-02-08 10:59:25 -0500

    rewrote group view

--- src/app/screens/Group/Group.tsx
+++ src/app/screens/Group/Group.tsx
@@ -1,143 +1,169 @@
 import {
   ActionIcon,
-  Avatar,
   Button,
-  CheckIcon,
   Container,
   CopyButton,
   Divider,
   Group as MantineGroup,
-  Overlay,
   Stack,
   Text,
   Title,
   Tooltip,
-  UnstyledButton,
 } from '@mantine/core';
+import { useEffect, useState } from 'react';
 import { RxCheck, RxClipboard } from 'react-icons/rx';
-import { useHover } from '@mantine/hooks';
-import { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { supabase } from 'src/app/supabase/client';
-import { SessionContext } from 'src/app/app';
-import { SiDiscord } from 'react-icons/si';
+import MemberPreview from './MemberPreview';
 
 // Takes group from url params and fetches group data
-function Group() {
-  const session = useContext(SessionContext);
-  const { invite_code } = useParams();
+function Group({ getGroups }: { getGroups: () => Promise<void> }) {
+  // Get group id from url params
+  const { group_id } = useParams();
+
+  // Set up state
   const [group, setGroup] = useState<Group | null>(null);
+  const [members, setMembers] = useState<GroupMember[] | null>(null);
+  const [groupError, setGroupError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // Navigate Hook
+  const navigate = useNavigate();
+
+  const fetchMembers = async (): Promise<GroupMember[] | null> => {
+    const { data, error } = await supabase.rpc('get_group_users', {
+      req_id: group_id,
+    });
+    if (data) {
+      return data;
+    } else {
+      console.log(error);
+      throw new Error('Could not fetch members');
+    }
+  };
 
   const fetchGroup = async () => {
     const { data, error } = await supabase
       .from('group')
       .select('*')
-      .eq('invite_code', invite_code);
+      .eq('id', group_id);
     if (error) {
+      console.log('Could not fetch group:' + error.message);
       console.log(error);
+      setGroupError('Could not fetch group');
     }
     if (data) {
-      setGroup(data[0]);
+      if (data[0] === undefined) {
+        console.log('Group not found');
+        setGroupError(
+          'Group not found, have you joined this group? If not, please check the invite code and try again.'
+        );
+        setLoading(false);
+        return;
+      } else {
+        setGroup(data[0]);
+        try {
+          const members = await fetchMembers();
+          setMembers(members);
+        } catch (error) {
+          console.log(error);
+          setGroupError('Could not fetch members');
+        }
+        setLoading(false);
+      }
     }
   };
 
   useEffect(() => {
     fetchGroup();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [group_id]);
 
-  return (
-    <Stack>
-      <MantineGroup position={'apart'}>
-        <Title>{group?.name}</Title>
-        <MantineGroup>
-          <Stack spacing={0}>
-            <Text style={{ fontWeight: 700 }}>Invite Code</Text>
-            <Text inline italic align="center">
-              {group?.invite_code}
-            </Text>
-          </Stack>
-          <CopyButton value="Invite Code">
-            {({ copied, copy }) => (
-              // Provided by the clipboard hook
-              <Tooltip
-                label={copied ? 'Copied' : 'Copy'}
-                withArrow
-                position="right"
-              >
-                <ActionIcon color={copied ? 'teal' : 'gray'} onClick={copy}>
-                  {copied ? <RxCheck size={16} /> : <RxClipboard size={16} />}
-                </ActionIcon>
-              </Tooltip>
-            )}
-          </CopyButton>
+  if (loading) {
+    return <Container size="sm">Loading...</Container>;
+  } else if (groupError) {
+    return (
+      <Container size="sm">
+        <Text>{groupError}</Text>
+      </Container>
+    );
+  } else {
+    return (
+      <Stack>
+        <MantineGroup position={'apart'}>
+          <Title>{group?.name}</Title>
+          <MantineGroup>
+            <Button
+              onClick={async () => {
+                const { data, error } = await supabase.rpc('leave_group', {
+                  req_id: group_id,
+                });
+                if (error) {
+                  console.log(error);
+                }
+                if (data) {
+                  console.log(data);
+                  await getGroups();
+                  // TODO: Add a toast to confirm leaving group
+                  navigate('/groups');
+                }
+              }}
+            >
+              Leave
+            </Button>
+            <Stack spacing={0}>
+              <Text style={{ fontWeight: 700 }}>Invite Code</Text>
+              <Text inline italic align="center">
+                {group?.invite_code}
+              </Text>
+            </Stack>
+            <CopyButton value="Invite Code">
+              {({ copied, copy }) => (
+                // Provided by the clipboard hook
+                <Tooltip
+                  label={copied ? 'Copied' : 'Copy'}
+                  withArrow
+                  position="right"
+                >
+                  <ActionIcon color={copied ? 'teal' : 'gray'} onClick={copy}>
+                    {copied ? <RxCheck size={16} /> : <RxClipboard size={16} />}
+                  </ActionIcon>
+                </Tooltip>
+              )}
+            </CopyButton>
+          </MantineGroup>
         </MantineGroup>
-      </MantineGroup>
-      <Divider />
+        <Divider />
 
-      {session?.user ? (
-        <UserPreview
-          user={{
-            id: session?.user.id,
-            full_name: session?.user.user_metadata.full_name,
-            name: session?.user.user_metadata.name,
-            avatar_url: session?.user.user_metadata.avatar_url,
+        <Button
+          onClick={async () => {
+            const { data, error } = await supabase.rpc('get_group_users', {
+              req_id: group_id,
+            });
+            if (error) console.log(error);
+            if (data) console.log(data);
           }}
-          key={session.user.id}
-        />
-      ) : null}
-    </Stack>
-  );
+        >
+          GetGroup
+        </Button>
+
+        {members
+          ?.sort((a, b) => {
+            // Sort Owner to top, then sort by full name
+            if (a.is_owner && !b.is_owner) {
+              return -1;
+            } else if (!a.is_owner && b.is_owner) {
+              return 1;
+            } else {
+              return a.full_name.localeCompare(b.full_name);
+            }
+          })
+          .map((member) => {
+            return <MemberPreview {...member} key={member.name} />;
+          })}
+      </Stack>
+    );
+  }
 }
 
 export default Group;
-
-export const UserPreview = ({
-  user,
-}: {
-  user: {
-    id: string;
-    full_name: string;
-    name: string;
-    avatar_url: string;
-  };
-}) => {
-  const { hovered, ref } = useHover();
-  return (
-    <Stack>
-      <MantineGroup position={'apart'}>
-        <MantineGroup>
-          <UnstyledButton
-            onClick={() => {
-              window.open(`https://discord.com/users/${user.name}`, '_blank');
-            }}
-          >
-            <Avatar
-              ref={ref}
-              src={hovered ? undefined : user.avatar_url}
-              alt={user.full_name}
-              size="xl"
-              radius="xl"
-
-              // color={hovered ? 'teal' : 'gray'}
-            >
-              <SiDiscord style={{ width: '100%' }} color={'#5865F2'} />
-            </Avatar>
-          </UnstyledButton>
-
-          <Stack spacing={0}>
-            <Text style={{ fontWeight: 700 }}>{user.full_name}</Text>
-            <Text inline italic align="center">
-              {user.name}
-            </Text>
-          </Stack>
-        </MantineGroup>
-        <MantineGroup>
-          <Text style={{ fontWeight: 700 }}>Survey Complete?</Text>
-          <RxCheck size={30} />
-        </MantineGroup>
-      </MantineGroup>
-      <Divider />
-    </Stack>
-  );
-};
