02ed7ca0fc6de52d38c48291901e76ae54f932d2
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-03-13 08:30:36 -0400

    updated styles

--- src/app/nav/AppNavbar/AppNavbar.tsx
+++ src/app/nav/AppNavbar/AppNavbar.tsx
@@ -109,24 +109,61 @@ export function AppNavbar({
 
   const theme = useMantineTheme();
 
+  const navLinkTheme = {
+    width: '100%',
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+    fontSize: theme.fontSizes.sm,
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[1]
+        : theme.colors.gray[7],
+    padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+    borderRadius: theme.radius.sm,
+    fontWeight: 500,
+  };
+
+  const navLinkHoverTheme = {
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    },
+  };
+
+  const navLinkIconTheme = {
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[2]
+        : theme.colors.gray[6],
+    marginRight: theme.spacing.sm,
+  };
+
+  const navLinkActiveTheme = {
+    backgroundColor: theme.fn.variant({
+      variant: 'light',
+      color: theme.primaryColor,
+    }).background,
+    color: theme.fn.variant({
+      variant: 'light',
+      color: theme.primaryColor,
+    }).color,
+  };
+
   return (
-    <Navbar
-      sx={{
-        backgroundColor:
-          theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
-        paddingBottom: 0,
-      }}
-      width={{ sm: 300 }}
-      p="md"
-      hiddenBreakpoint="sm"
-    >
+    <Navbar width={{ sm: 300 }} p="md" hiddenBreakpoint="sm">
       {/* Links to groups and a shortcut to the contents page */}
-      <Navbar.Section grow component={ScrollArea}>
+      <Navbar.Section grow w="100%" component={ScrollArea}>
         <Title order={3} mb="xs">
           Groups
         </Title>
         {groups?.map((group) => (
           <Box
+            p={'xs'}
             component={NavLink}
             to={`/group/${group.id}`}
             key={group.id}
@@ -134,92 +171,69 @@ export function AppNavbar({
               getGroups();
               setNavOpen(false);
             }}
-            sx={{
-              cursor: 'pointer',
-              display: 'flex',
-              alignItems: 'center',
-              textDecoration: 'none',
-              fontSize: theme.fontSizes.sm,
-              color:
-                theme.colorScheme === 'dark'
-                  ? theme.colors.dark[1]
-                  : theme.colors.gray[7],
-              padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-              borderRadius: theme.radius.sm,
-              fontWeight: 500,
-              hover: {
-                backgroundColor:
-                  theme.colorScheme === 'dark'
-                    ? theme.colors.dark[6]
-                    : theme.colors.gray[0],
-              },
-            }}
+            sx={[
+              navLinkTheme,
+              navLinkHoverTheme,
+              location.pathname.includes(`/group/${group.id}`)
+                ? navLinkActiveTheme
+                : {},
+            ]}
           >
-            <Group
-              sx={
-                location.pathname.includes(`/group/${group.id}`)
-                  ? {
-                      backgroundColor: theme.fn.variant({
-                        variant: 'light',
-                        color: theme.primaryColor,
-                      }).background,
-                      color: theme.fn.variant({
-                        variant: 'light',
-                        color: theme.primaryColor,
-                      }).color,
-                    }
-                  : {}
-              }
-            >
-              <Box
-                sx={{
-                  color:
-                    theme.colorScheme === 'dark'
-                      ? theme.colors.dark[2]
-                      : theme.colors.gray[6],
-                  marginRight: theme.spacing.sm,
-                }}
-                component={FaDiceD20}
-              />
+            <Group>
+              <Box sx={navLinkIconTheme} component={FaDiceD20} />
               {group.name}
             </Group>
           </Box>
         ))}
       </Navbar.Section>
       <Navbar.Section>
-        <NavLink
-          // className={cx(classes.link, {
-          // [classes.linkActive]: location.pathname.includes(`/contents`),
-          // })}
+        <Box
+          p={'xs'}
+          component={NavLink}
           to="/contents"
           key="contents"
           onClick={() => {
             getGroups();
             setNavOpen(false);
           }}
+          sx={[
+            navLinkTheme,
+            navLinkHoverTheme,
+            location.pathname.includes(`/contents`) ? navLinkActiveTheme : {},
+          ]}
         >
-          <Group style={{ fontWeight: 700 }}>
-            <RiChatSettingsFill />
+          <Group>
+            <Box sx={navLinkIconTheme} component={RiChatSettingsFill} />
             Content Preferences
           </Group>
-        </NavLink>
+        </Box>
       </Navbar.Section>
+
       {/* Join box and create new group button */}
-      <Navbar.Section mb={'md'}>
-        <NavLink
-          style={{ marginBottom: '1rem' }}
-          // className={cx(classes.link, {
-          // [classes.linkActive]: location.pathname.includes(`/create`),
-          // })}
-          onClick={() => setNavOpen(false)}
-          to="/create"
-        >
-          <Group style={{ fontWeight: 700 }}>
-            <FaPlus />
-            Create New Group
-          </Group>
-        </NavLink>
-      </Navbar.Section>
+
+      <Box
+        p={'xs'}
+        component={NavLink}
+        to="/create"
+        key="create"
+        mb={'md'}
+        onClick={() => {
+          getGroups();
+          setNavOpen(false);
+        }}
+        sx={[
+          navLinkTheme,
+          navLinkHoverTheme,
+          location.pathname.includes(`/create`) ? navLinkActiveTheme : {},
+        ]}
+      >
+        <Group>
+          <Box sx={[navLinkIconTheme]} component={FaPlus} />
+          Create New Group
+        </Group>
+      </Box>
+
+      {/* </Navbar.Section> */}
       <Navbar.Section mb={'md'}>
         <JoinBox getGroups={getGroups} />
       </Navbar.Section>
