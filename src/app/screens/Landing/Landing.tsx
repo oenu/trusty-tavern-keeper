de16dff1b03eb6f866e81818acfb72f3bdcff59a
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2023-03-13 08:11:56 -0400

    init landing text

--- src/app/screens/Landing/Landing.tsx
+++ src/app/screens/Landing/Landing.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import { Stack, Text, Title } from '@mantine/core';
 
 function Landing() {
-  return <div>Landing</div>;
+  return (
+    <Stack>
+      <Title
+        style={{
+          // fontSize: rem(62)
+          fontWeight: 900,
+        }}
+      >
+        Enjoy D&D games that match your style and boundaries
+      </Title>
+      <Text>
+        Help your DM create a game that you'll love by choosing content and
+        roleplay options that fit your expectations and comfort level.
+      </Text>
+      <Text>
+        Sure, I can write a longer product description for you. How about this:
+        Trusty Tavern Keeper is a website that helps you and your DM create the
+        best D&D games online. It lets you choose content and roleplay options
+        that fit your expectations and comfort level. Whether you prefer casual
+        or immersive roleplay, whether you are OK with violence or romance,
+        whether you like fantasy or sci-fi settings, Trusty Tavern Keeper has
+        you covered.
+      </Text>
+      <Text>
+        All you need to do is sign up with your Discord account and join a
+        group. You will then be asked to fill out a short and anonymous survey
+        where you can select your preferences and boundaries for each game. Your
+        DM will also fill out the survey and get a report with the group's
+        aggregated results. The report will show the content and roleplay
+        options that are most popular, most controversial, and most avoided by
+        the group. It will also provide suggestions for the DM on how to create
+        a game that everyone will enjoy.
+      </Text>
+      <Text>
+        Trusty Tavern Keeper is designed to make online D&D games more fun and
+        satisfying for both players and DMs. It helps you avoid awkward
+        situations, misunderstandings, and conflicts that can ruin your gaming
+        experience. It also helps you discover new genres, themes, and styles of
+        D&D that you might not have tried before.
+      </Text>
+      and completing a quick and anonymous survey. You can pick content and
+      roleplay options that fit your expectations and comfort zones. You and
+      your DM will get a report with the group's summary results and advice for
+      the perfect game.
+    </Stack>
+  );
 }
 
 export default Landing;
